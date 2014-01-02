f7e03b7feefe96ae6f803483f58c7a6bb1b70eac
Zoli Kahan <zolikahan@gmail.com>
2014-01-02 13:29:44 -0600

    updated tests for new dictionary

--- src/test/boggle.js
+++ src/test/boggle.js
@@ -9,31 +9,31 @@ gt.test('basics', function () {
 gt.test('as and bs', function () {
   var words = boggle('AABBBBBBBBBBBBBB');
   gt.array(words, 'got array of words');
-  gt.equal(words.length, 6, 'couple of words');
+  gt.equal(words.length, 4, 'couple of words');
 });
 
 gt.test('as and bs with spaces', function () {
   var words = boggle('AABB BBBB BBBB BBBB');
   gt.array(words, 'got array of words');
-  gt.equal(words.length, 6, 'couple of words');
+  gt.equal(words.length, 4, 'couple of words');
 });
 
 gt.test('as and bs with weird spaces', function () {
   var words = boggle('  AABB    BB  BB B BB B BBB B ');
   gt.array(words, 'got array of words');
-  gt.equal(words.length, 6, 'couple of words');
+  gt.equal(words.length, 4, 'couple of words');
 });
 
 gt.test('alphabet', function () {
   var words = boggle('abcd efgh ijkl mnop');
   gt.array(words, 'got array of words');
-  gt.equal(words.length, 34, 'couple of words');
+  gt.equal(words.length, 18, 'couple of words');
 });
 
 gt.test('helios', function () {
   var words = boggle('heli zzzo zzzs zzzz');
   gt.array(words, 'got array of words');
-  gt.equal(words.length, 13, 'couple of words');
+  gt.equal(words.length, 11, 'couple of words');
 });
 
 gt.test('Qu', function() {
